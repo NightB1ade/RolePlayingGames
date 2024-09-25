1a63a231c1b97a17f22564ce5238e935a7437b98
NightB1ade <nightb1ade@users.noreply.github.com>
2024-09-25 21:05:21 +0100

    Generic: AMFES: Filter improvement, keep section if match

--- Generic/AMonsterForEverySeason.js
+++ Generic/AMonsterForEverySeason.js
@@ -1,38 +1,37 @@
 function FilterUpdate() {
 	var filterText = document.getElementById("Filter").value.toLowerCase();
 	var tokenSection = document.getElementsByClassName("TokenSection");
-	var tokenRows = document.getElementsByClassName("TokenRow");
 
 	for (i = 0; i < tokenSection.length; i++) {
 		var match = 0;
-		var names = tokenSection[i].getElementsByClassName("TokenName");
+		var tokenRows = tokenSection[i].getElementsByClassName("TokenRow");
 
-		for (j = 0; j < names.length; j++) {
-			var name = names[j].textContent;
-
-			if (name.toLowerCase().indexOf(filterText) > -1) {
-				match = 1;
-				break
-			}
-		}
-
-		if (match == 1) {
+		if (
+			tokenSection[i].getElementsByTagName("h1").textContent
+				.toLowerCase().indexOf(filterText) > -1
+		) {
 			tokenSection[i].style.display = "";
+
+			for (j = 0; j < tokenRows.length; j++) {
+				tokenRows[j].style.display = "";
+			}
 		} else {
-			tokenSection[i].style.display = "none";
-		}
-	}
+			for (j = 0; j < tokenRows.length; j++) {
+				var tokenRow = tokenRows[j];
+				var name = tokenRow.getElementsByTagName("td")[0].textContent;
 
-	for (i = 0; i < tokenRows.length; i++) {
-		var td = tokenRows[i].getElementsByTagName("td")[0];
+				if (name.toLowerCase().indexOf(filterText) > -1) {
+					tokenRow.style.display = "";
+					match = 1;
+				} else {
+					tokenRow.style.display = "none";
+				}
+			}
 
-		if (td) {
-			var txt = td.textContent;
-
-			if (txt.toLowerCase().indexOf(filterText) > -1) {
-				tokenRows[i].style.display = "";
+			if (match == 1) {
+				tokenSection[i].style.display = "";
 			} else {
-				tokenRows[i].style.display = "none";
+				tokenSection[i].style.display = "none";
 			}
 		}
 	}
